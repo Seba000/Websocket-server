dd333dbb8884bf0a0f7888b731aa85f1bcb1c18b
Sebastian Letelier <sebalete10@gmail.com>
2022-12-16 21:42:40 -0300

    Estructura inicial

---
+++ public/js/socket-client.js
@@ -0,0 +1,20 @@
+//ref
+const lblOnline = document.querySelector('#lblOnline')
+const lblOffline = document.querySelector('#lblOffline')
+
+const socket = io();
+
+socket.on('connect', ()=> {
+    console.log('conectado');
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+    console.log('desconectado');
+
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = '';
+
+});
